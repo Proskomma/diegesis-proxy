ece6958a20629ec2b34b580ab0a5004a84df27c3
Mark Howe <mvahowe@gmail.com>
2021-12-14 11:53:25 +0100

    Set cv for all nodes with nodeId

--- scripts/rework_low_fat_node_ids.js
+++ scripts/rework_low_fat_node_ids.js
@@ -1,33 +1,33 @@
 const path = require('path');
 const fse = require('fs-extra');
 const fileNames = [
-    ["01-matthew", "MAT"],
-    ["02-mark", "MRK"],
-    ["03-luke", "LUK"],
-    ["04-john", "JHN"],
-    ["05-acts", "ACT"],
-    ["06-romans", "ROM"],
-    ["07-1corinthians", "1CO"],
-    ["08-2corinthians", "2CO"],
-    ["09-galatians", "GAL"],
-    ["10-ephesians", "EPH"],
-    ["11-philippians", "PHP"],
-    ["12-colossians", "COL"],
-    ["13-1thessalonians", "1TH"],
-    ["14-2thessalonians", "2TH"],
-    ["15-1timothy", "1TI"],
-    ["16-2timothy", "2TI"],
-    ["17-titus", "TIT"],
-    ["18-philemon", "PHM"],
-    ["19-hebrews", "HEB"],
-    ["20-james", "JAS"],
-    ["21-1peter", "1PE"],
-    ["22-2peter", "2PE"],
-    ["23-1john", "1JN"],
-    ["24-2john", "2JN"],
-    ["25-3john", "3JN"],
-    ["26-jude", "JUD"],
-    ["27-revelation", "REV"],
+    ['01-matthew', 'MAT'],
+    ['02-mark', 'MRK'],
+    ['03-luke', 'LUK'],
+    ['04-john', 'JHN'],
+    ['05-acts', 'ACT'],
+    ['06-romans', 'ROM'],
+    ['07-1corinthians', '1CO'],
+    ['08-2corinthians', '2CO'],
+    ['09-galatians', 'GAL'],
+    ['10-ephesians', 'EPH'],
+    ['11-philippians', 'PHP'],
+    ['12-colossians', 'COL'],
+    ['13-1thessalonians', '1TH'],
+    ['14-2thessalonians', '2TH'],
+    ['15-1timothy', '1TI'],
+    ['16-2timothy', '2TI'],
+    ['17-titus', 'TIT'],
+    ['18-philemon', 'PHM'],
+    ['19-hebrews', 'HEB'],
+    ['20-james', 'JAS'],
+    ['21-1peter', '1PE'],
+    ['22-2peter', '2PE'],
+    ['23-1john', '1JN'],
+    ['24-2john', '2JN'],
+    ['25-3john', '3JN'],
+    ['26-jude', 'JUD'],
+    ['27-revelation', 'REV'],
 ];
 
 const reworkNodeIds = (ob, cv) => {
@@ -47,10 +47,8 @@ const reworkNodeIds = (ob, cv) => {
         const chapter = parseInt(nodeId.substring(2, 5)).toString();
         const verse = parseInt(nodeId.substring(5, 8)).toString();
         const newCV = `${chapter}:${verse}`;
-        if (true || newCV !== cv) {
-            ret.content.cv = newCV;
-            cv = newCV;
-        }
+        ret.content.cv = newCV;
+        cv = newCV;
     }
     if (ret.children) {
         ret.children = ob.children
